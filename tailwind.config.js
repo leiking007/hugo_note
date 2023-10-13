9e4cb51ea35bfdff024925932f3b216c5ce45b29
lei <lei@123.com>
2023-10-13 23:48:18 +0800

    1

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./content/**/*.md","./themes/hugo-tl/layouts/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      'sm':['0.8rem',{
+        lineHeight: '1.2',
+        fontWeight: '500',
+      }],
+      'base': ['1rem',{
+        lineHeight: '1.6',
+        fontWeight: '500'
+      }],
+      'xl': ['1.25rem',{
+        lineHeight: '1.6',
+        fontWeight: '500'
+      }],
+      '2xl': ['1.5rem', {
+        lineHeight: '2',
+        fontWeight: '500',
+      }],
+      '3xl': ['1.875rem', {
+        lineHeight: '2.25',
+        fontWeight: '700',
+      }],
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": {content: ''},
+            "code::after": {content: ''}
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
+
